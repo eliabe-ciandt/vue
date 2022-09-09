74298bab04ebdee6b1ee6fe86451834f6d5c8d09
Eliabe-ciandt <eliabe@ciandt.com>
2022-09-09 16:52:37 -0300

    Creates components and LifeCycle events

--- horaDeCodar/02_projeto_vue/src/App.vue
+++ horaDeCodar/02_projeto_vue/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <PrimeiroComponente />
+    <p>O dado de app é: {{ testando }}</p>
+    <LifeCycle />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+    LifeCycle,
+  },
+  data() {
+    return {
+      testando: "teste",
+    };
+  },
+};
+</script>
