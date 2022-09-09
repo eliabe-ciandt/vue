3eebdb9087b94f295c0247dd5d64d8f667cfcb3c
Eliabe-ciandt <eliabe@ciandt.com>
2022-09-09 17:48:36 -0300

    Create Pessoa component

---
+++ horaDeCodar/02_projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Esta é uma descrição da pessoa {{ nome }}</h2>
+    <Info />
+    <p>Mas também sei:</p>
+    <ol>
+      <li>Python</li>
+      <li>Java</li>
+      <li>Spring</li>
+      <li>Bancos de dados SQL</li>
+      <li>etc.</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import Info from "./info.vue";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Pessoa",
+  components: {
+    Info,
+  },
+  data() {
+    return {
+      nome: "que vos fala",
+    };
+  },
+};
+</script>
